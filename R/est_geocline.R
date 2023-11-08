6b7a1d60c97d2e026f89cb29d120cf5f3555ce26
zgompert <zach.gompert@usu.edu>
2023-11-07 21:02:27 -0700

    Working on geographic clines

--- R/est_geocline.R
+++ R/est_geocline.R
@@ -1,29 +1,42 @@
 #' Function to estimate geographic clines for a set of genetic loci or hybrid index
 #'
 #' Uses Hamiltonian Monte Carlo (HMC) for Bayesian inference of geographic clines from genetic data. 
-#' @param Gx genetic data for putative hybrids
-#' @param G0 genetic data for parental reference set 0
-#' @param G1 genetic data for parental reference set 1
-#' @param p0 allele frequencies for parental reference set 0
-#' @param p1 allele frequencies for parental reference set 1
+#' @param G genetic data 
+#' @param P allele frequency data
+#' @param Geo geographic coordinates for the sampled populations, these will be centered automatically
+#' @param Ids indexes designating which population each individual belongs to, indexes run from 1 to the number of populations
 #' @param model genotype, glik, or ancestry
 #' @param ploidy diploid or mixed
 #' @param pldat ploidy data for mixed ploidy
+#' @param prec precision for known allele frequencies, set to approximately 1/2N
 #'
 #' @return list of parameter estimates and full HMC results from stan
 #'
 #' @export
-est_geocl<-function(G=NULL,Geo=NULL,Ids=NULL,model="genotype",ploidy="diploid",pldat=NULL){
+est_geocl<-function(G=NULL,P=NULL,Geo=NULL,Ids=NULL,model="genotype",ploidy="diploid",pldat=NULL,prec=0.001){
 
-   options(mc.cores = parallel::detectCores())
+	options(mc.cores = parallel::detectCores())
         
-	dat<-list(L=dim(G)[2],N=dim(G)[1],J=length(Geo),G=G,geo=Geo,popid=Ids)
+        if(is.null(P)){ 
+        	## need to first estimate population allele frequencies
+        	## use analytical solution for the posterior
+        	P<-matrix(NA,nrow=length(Geo),ncol=dim(G)[2])
+        	for(i in 1:dim(G)[2]){
+        		Y<-tapply(X=G[,i],INDEX=Ids,sum)
+        		N<-tapply(X=G[,i]>=0,INDEX=Ids,sum)
+        		P[,i]<-(Y+0.5)/(2 * N + 1)
+        	}
+	} else{
+		P[P < prec]<-prec
+		P[P > (1-prec)]<-1-prec
+	}
+		        
+	dat<-list(L=dim(P)[2],J=dim(P)[1],P=P,geo=Geo)
 	fit<-rstan::sampling(stanmodels$geocline,data=dat)
 	w<-t(apply(rstan::extract(fit,"w")[[1]],2,quantile,probs=c(.5,.025,.05,.95,.975)))
 	mu<-quantile(rstan::extract(fit,"mu")[[1]],probs=c(.5,.025,.05,.95,.975))
-    sigma<-quantile(rstan::extract(fit,"sigma")[[1]],probs=c(.5,.025,.05,.95,.975))
+	sigma<-quantile(rstan::extract(fit,"sigma")[[1]],probs=c(.5,.025,.05,.95,.975))
 	## create a list with parameter estimates plus full hmc object
-    geoout<-list(w=w,mu=mu,sigma=sigma,geo_hmc=fit)
-
+	geoout<-list(w=w,mu=mu,sigma=sigma,geo_hmc=fit)
 	return(geoout)
 }
--- inst/stan/geocline.stan
+++ inst/stan/geocline.stan
@@ -1,15 +1,13 @@
 
 data{
 	int L; /* # of loci */
-	int N; /* # of organisms */
 	int J; /* # of populations */
-    int<lower=0> popid[N]; /* population ID index */
-    real geo[J]; /* 1D array of geographic locations */
-	int<lower=0, upper=2> G[N, L]; /* 2D array of G */
+	real geo[J]; /* 1D array of geographic locations */
+	real<lower=0, upper=1> P[J, L]; /* 2D array of population allele frequencies */	
 }
 
 parameters{
-	real<lower=0, upper=1> P[J, L]; /* 2D array of population allele frequencies */
+	
     vector[L] slope; /* vector of slopes */
     vector[L] cent; /* vector of intercepts */
     real mu; /* mean for widths, w */
@@ -24,25 +22,18 @@ transformed parameters{
     for(i in 1:L){
         w[i] = 1/(0.25 * slope[i]);
     }
-    sdg = 2 * sd(geo);
+    sdg = 3 * sd(geo);
     
 }    
 
 model{
-
-	for(i in 1:L){
-		for(j in 1:N){
-			/* increment likelihood for genotype*/
-			target += binomial_lpmf(G[j,i] | 2, P[popid[j],i]);
-		}
-	}
 	for(i in 1:L){
 	    for(j in 1:J){
-    		/* increment linear models */
+    		/* increment likelihood */
     		target += normal_lpdf(logit(P[j,i]) | cent[i] + slope[i] * geo[j], serr);
     	}	
 	}
-	/* increment remaining priors */
+	/* increment priors */
 	for(i in 1:L){
 	    target += normal_lpdf(slope[i] | mu, sigma);
 	    target += normal_lpdf(cent[i] | 0, sdg);

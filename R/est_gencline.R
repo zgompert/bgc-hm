cde78f5ebc61e0e904b28e7f1318c9b58a8978fd
zgompert <zach.gompert@usu.edu>
2023-12-05 10:23:41 -0700

    added documentation

--- R/est_gencline.R
+++ R/est_gencline.R
@@ -1,27 +1,50 @@
-#' Estimate genomic clines using a hierarchical model with unknown cline variances 
+#' Estimate genomic clines using Bayesian HMC
 #'
-#' Uses Hamiltonian Monte Carlo (HMC) for Bayesian inference of genomic clines from genetic data. This fits a hierarchical log-logistic model for genomic clines with two key parameters, cline center and cline gradient (i.e., slope, inversely proportional to cline width). The model also estimates the cline SDs across loci, SDc (variation in logit centers) and SDv (variation in log10 gradients). 
-#' @param Gx genetic data for putative hybrids, matrix for known genotypes, list of matrixes for genotype likelihoods, matrix of ancestry for the ancestry model
-#' @param G0 genetic data for parental reference set 0
-#' @param G1 genetic data for parental reference set 1
-#' @param p0 allele frequencies for parental reference set 0
-#' @param p1 allele frequencies for parental reference set 1
-#' @param H hybrid indexes for the putative hybrids
-#' @param model genotype, glik, or ancestry
-#' @param ploidy diploid or mixed
-#' @param pldat ploidy data for mixed ploidy
-#' @param hier boolean, fit hierarchical model (TRUE) or assume cline SDs known (FALSE)
-#' @param SDc known cline center SD on logit scale
-#' @param SDv known cline gradient SD on log10 scale
-#' @param n_chains number of HMC chains for posterior inference
+#' Uses Hamiltonian Monte Carlo (HMC) for Bayesian inference of genomic clines from genetic data. This fits a hierarchical log-logistic model for genomic clines with two key parameters, cline center and cline gradient (i.e., slope, inversely proportional to cline width). The model also estimates the cline standard deviations (SDs) across loci, SDc = variation in logit centers and SDv = variation in log10 gradients. 
+#' @param Gx genetic data for putative hybrids in the form of a matrix for known genotypes (rows = individuals, columns = loci), a list of matrixes for genotype likelihoods (same dimensions but one matrix per genotype), or  matrix of ancestry for the ancestry model (rows = individuals, columns = loci).
+#' @param G0 genetic data for parental reference set 0 formatted as described for Gx.
+#' @param G1 genetic data for parental reference set 1 formatted as described for Gx.
+#' @param p0 vector of allele frequencies for parental reference set 0 (one entry per locus).
+#' @param p1 vector allele frequencies for parental reference set 1 (one entry per locus).
+#' @param H vector of hybrid indexes for the putative hybrids (one entry per locus).
+#' @param model for genetic data, either 'genotype' for known gentoypes, 'glik' for genotype likelihoods, or 'ancestry' for known ancestry.
+#' @param ploidy species ploidy, either all 'diploid' or 'mixed' for diploid and haploid loci or individuals.
+#' @param pldat matrix or list of matrixes of ploidy data for mixed ploidy (rows = individuals, columns = loci) indicating ploidy (2 = diploid, 1 = haploid).
+#' @param hier Boolean, fit hierarchical model (TRUE) that estimates cline SDs or non-hierarchical model that assumes cline SDs are known (FALSE).
+#' @param SDc known cline center SD on logit scale.
+#' @param SDv known cline gradient SD on log10 scale.
+#' @param n_chains number of HMC chains for posterior inference.
+#' @param n_iters A positive integer specifying the number of iterations for each chain (including warmup), default is 2000.
+#' @param p_warmup proportion (between 0 and 1) of n_iters to use as warmup (i.e., burnin), default is 0.5.
+#' @param n_thin positive integer, save every n_thin HMC iterations for the posterior, default is 1.
+#' @param n_cores number of cores to use for HMC, leave as NULL to automatically detect the number of cores present (no more than n_chains cores can be used even if available).
 #'
-#' @return list of parameter estimates and full HMC results from stan, this includes cline parameters (center and gradient) and standard deviations describing variability in clines across loci (SDc and SDv) 
+#' @details
+#' Clines can be inferred based on known genotypes (model = 'genotype'), genotype likelihoods, (model = 'glik') or known (estimated) ancestry (model = 'ancestry'). Genotypes should be encoded as 0 (homozygote), 1 (heterozygote) and 2 (alternative homozygote). No specific polarization (e.g., minor allele, reference allele, etc.) of 0 vs 2 is required. For haploid loci, you can use 0 and 1 or 0 and 2. Genotype likelihoods should be on their natural scale (not phred scaled) and the values for each locus for an individual should sum to 1. The data should be provided as a list of three matrixes, with the matrixes giving the likelihoods for genotypes 0, 1 and 2 respectively. Thus, each matrix will have one row per individual and one column per locus. For haploid loci with genotype likelihoods, you must use the 0 and 2 matrixes to store the likelihoods of the two possible states. For the ancestry model, clines are inferred directly from known local (locus-specific) ancestry rather than from genotype data. Users are free to use whatever software they prefer for local ancestry inference (many exist). In this case, each entry in the individual (rows) by locus (columns) matrix should denote the number of gene copies inherited from parental population 1 (where pure parent 1 corresponds with a hybrid index of 1 and pure parent 0 corresponds with a hybrid index of 0). Haploids can be encoded using 0 and 1 or 0 and 2 (this is treated equivalently). 
+#' @details
+#' Hybrid genetic (or ancestry) data are always required. For genotype or genotype likelihood models, users must either provide pre-estimated parental allele frequencies or parent genetic (genotypes or genotype likelihoods) that can be used to infer allele frequencies. Parental data are not required for the ancestry model.
+#' @details
+#' Ploidy data are only required for the mixed ploidy data. In this case, there should be one matrix for the hybrids or a list of matrixes for the hybrids (1st matrix) and each parent (2nd and 3rd matrixes, with parent 0 first). The latter is required for the genotype or genotype likelihood models if parental allele frequencies are not provided. The matrixes indicate whether each locus (column) for each individual (row) is diploid (2) or haploid (1).
 #'
+#' @return A list of parameter estimates and full HMC results from stan, this includes cline parameters (center and gradient), and, for hierarchical models, standard deviations describing variability in clines across loci (SDc and SDv). Parameter estimates are provided as a point estimate (median of the posterior) and 95% equal-tail probability intervals (2.5th and 97.5th quantiles of the posterior distribution). These are provided as a vector or matrix depending on the dimensionality of the parameter. The full HMC output from rstan is provided as the final element in the list. This can be used for HMC diagnostics and to extract other model outputs not provided by default. 
+#'
+#' @seealso 'rstan::stan' for details on HMC with stan and the rstan HMC output object.
+#'
+#' @references
+#' Gompert Z, et al. 2024. Bayesian hybrid zone analyses with Hamiltonian Monte Carlo in R. Manuscript in preparation
 #' @export
 est_genocl<-function(Gx=NULL,G0=NULL,G1=NULL,p0=NULL,p1=NULL,H=NULL,model="genotype",ploidy="diploid",pldat=NULL,
-	hier=TRUE,SDc=NULL,SDv=NULL,n_chains=4){
+	hier=TRUE,SDc=NULL,SDv=NULL,n_chains=4,n_iters=2000,p_warmup=0.5,n_thin=1,n_cores=NULL){
 	
-	options(mc.cores = parallel::detectCores())
+	## get or set number of cores for HMC
+	if(is.null(n_cores)){
+		options(mc.cores = parallel::detectCores())
+	} else{
+		mc.cores<-n_cores
+	}
+	
+	## determine number of warmup iterations
+	n_warmup<-floor(p_warmup * n_iters)
 	
 	## sanity tests to make sure options match up
 	if(hier==FALSE & (is.null(SDc) | is.null(SDv))){
@@ -64,7 +87,8 @@ est_genocl<-function(Gx=NULL,G0=NULL,G1=NULL,p0=NULL,p1=NULL,H=NULL,model="genot
 
 		if(dim(Gx)[2]==1){stop("at least two loci required for the hierarchical model")}
 
-		fit<-rstan::sampling(stanmodels$gencline,data=dat)
+		fit<-rstan::sampling(stanmodels$gencline,data=dat,
+		iter=n_iters,warmup=p_warmup,thin=n_thin)
 		cc<-t(apply(rstan::extract(fit,"center")[[1]],2,quantile,probs=c(.5,.05,.95)))
 		cv<-t(apply(rstan::extract(fit,"v")[[1]],2,quantile,probs=c(.5,.05,.95)))
 		sdc<-quantile(rstan::extract(fit,"sdc")[[1]],probs=c(.5,.05,.95))
@@ -81,7 +105,8 @@ est_genocl<-function(Gx=NULL,G0=NULL,G1=NULL,p0=NULL,p1=NULL,H=NULL,model="genot
 			init_ll<-lapply(1:n_chains, function(id) initf(chain_id = id))
 			dat<-list(L=dim(Gx)[2],N=dim(Gx)[1],G=Gx,H=H,P0=p0,P1=p1,sc=SDc,sv=SDv,
 			init=init_ll)
-			fit<-rstan::sampling(stanmodels$gencline_sdk,data=dat)
+			fit<-rstan::sampling(stanmodels$gencline_sdk,data=dat,
+			iter=n_iters,warmup=p_warmup,thin=n_thin)
 			cc<-t(apply(rstan::extract(fit,"center")[[1]],2,quantile,probs=c(.5,.05,.95)))
 			cv<-t(apply(rstan::extract(fit,"v")[[1]],2,quantile,probs=c(.5,.05,.95)))
 
@@ -93,7 +118,8 @@ est_genocl<-function(Gx=NULL,G0=NULL,G1=NULL,p0=NULL,p1=NULL,H=NULL,model="genot
 			init_ll<-lapply(1:n_chains, function(id) initf(chain_id = id))		
 			dat<-list(L=1,N=length(Gx),G=Gx,H=H,P0=p0,P1=p1,sc=SDc,sv=SDv,
 			init=init_ll)
-			fit<-rstan::sampling(stanmodels$gencline_one,data=dat)
+			fit<-rstan::sampling(stanmodels$gencline_one,data=dat,
+			iter=n_iters,warmup=p_warmup,thin=n_thin)
 			cc<-quantile(rstan::extract(fit,"center")[[1]],probs=c(.5,.05,.95))
 			cv<-quantile(rstan::extract(fit,"v")[[1]],probs=c(.5,.05,.95))
 		}
@@ -112,7 +138,8 @@ est_genocl<-function(Gx=NULL,G0=NULL,G1=NULL,p0=NULL,p1=NULL,H=NULL,model="genot
 
 		if(dim(Gx[[1]])[2]==1){stop("at least two loci required for the hierarchical model")}
 
-		fit<-rstan::sampling(stanmodels$gencline_gl,data=dat)
+		fit<-rstan::sampling(stanmodels$gencline_gl,data=dat,
+			iter=n_iters,warmup=p_warmup,thin=n_thin)
 		cc<-t(apply(rstan::extract(fit,"center")[[1]],2,quantile,probs=c(.5,.05,.95)))
 		cv<-t(apply(rstan::extract(fit,"v")[[1]],2,quantile,probs=c(.5,.05,.95)))
 		sdc<-quantile(rstan::extract(fit,"sdc")[[1]],probs=c(.5,.05,.95))
@@ -130,7 +157,8 @@ est_genocl<-function(Gx=NULL,G0=NULL,G1=NULL,p0=NULL,p1=NULL,H=NULL,model="genot
 			dat<-list(L=dim(Gx[[1]])[2],N=dim(Gx[[1]])[1],
 			GL0=Gx[[1]],GL1=Gx[[2]],GL2=Gx[[3]],H=H,P0=p0,P1=p1,sc=SDc,sv=SDv,
 			init=init_ll)
-			fit<-rstan::sampling(stanmodels$gencline_sdk_gl,data=dat)
+			fit<-rstan::sampling(stanmodels$gencline_sdk_gl,data=dat,
+			iter=n_iters,warmup=p_warmup,thin=n_thin)
 			cc<-t(apply(rstan::extract(fit,"center")[[1]],2,quantile,probs=c(.5,.05,.95)))
 			cv<-t(apply(rstan::extract(fit,"v")[[1]],2,quantile,probs=c(.5,.05,.95)))
 
@@ -142,7 +170,8 @@ est_genocl<-function(Gx=NULL,G0=NULL,G1=NULL,p0=NULL,p1=NULL,H=NULL,model="genot
 			init_ll<-lapply(1:n_chains, function(id) initf(chain_id = id))		
 			dat<-list(L=1,N=length(Gx[[1]]),GL0=Gx[[1]],GL1=Gx[[2]],GL2=Gx[[3]],
 			H=H,P0=p0,P1=p1,sc=SDc,sv=SDv,init=init_ll)
-			fit<-rstan::sampling(stanmodels$gencline_one_gl,data=dat)
+			fit<-rstan::sampling(stanmodels$gencline_one_gl,data=dat,
+			iter=n_iters,warmup=p_warmup,thin=n_thin)
 			cc<-quantile(rstan::extract(fit,"center")[[1]],probs=c(.5,.05,.95))
 			cv<-quantile(rstan::extract(fit,"v")[[1]],probs=c(.5,.05,.95))
 		}
@@ -159,7 +188,8 @@ est_genocl<-function(Gx=NULL,G0=NULL,G1=NULL,p0=NULL,p1=NULL,H=NULL,model="genot
 
 		if(dim(Gx)[2]==1){stop("at least two loci required for the hierarchical model")}
 
-		fit<-rstan::sampling(stanmodels$gencline_z,data=dat)
+		fit<-rstan::sampling(stanmodels$gencline_z,data=dat,
+			iter=n_iters,warmup=p_warmup,thin=n_thin)
 		cc<-t(apply(rstan::extract(fit,"center")[[1]],2,quantile,probs=c(.5,.05,.95)))
 		cv<-t(apply(rstan::extract(fit,"v")[[1]],2,quantile,probs=c(.5,.05,.95)))
 		sdc<-quantile(rstan::extract(fit,"sdc")[[1]],probs=c(.5,.05,.95))
@@ -176,7 +206,8 @@ est_genocl<-function(Gx=NULL,G0=NULL,G1=NULL,p0=NULL,p1=NULL,H=NULL,model="genot
 			init_ll<-lapply(1:n_chains, function(id) initf(chain_id = id))
 			dat<-list(L=dim(Gx)[2],N=dim(Gx)[1],Z=Gx,H=H,sc=SDc,sv=SDv,
 			init=init_ll)
-			fit<-rstan::sampling(stanmodels$gencline_sdk_z,data=dat)
+			fit<-rstan::sampling(stanmodels$gencline_sdk_z,data=dat,
+			iter=n_iters,warmup=p_warmup,thin=n_thin)
 			cc<-t(apply(rstan::extract(fit,"center")[[1]],2,quantile,probs=c(.5,.05,.95)))
 			cv<-t(apply(rstan::extract(fit,"v")[[1]],2,quantile,probs=c(.5,.05,.95)))
 
@@ -188,7 +219,8 @@ est_genocl<-function(Gx=NULL,G0=NULL,G1=NULL,p0=NULL,p1=NULL,H=NULL,model="genot
 			init_ll<-lapply(1:n_chains, function(id) initf(chain_id = id))		
 			dat<-list(L=1,N=length(Gx),Z=Gx,H=H,sc=SDc,sv=SDv,
 			init=init_ll)
-			fit<-rstan::sampling(stanmodels$gencline_one_z,data=dat)
+			fit<-rstan::sampling(stanmodels$gencline_one_z,data=dat,
+			iter=n_iters,warmup=p_warmup,thin=n_thin)
 			cc<-quantile(rstan::extract(fit,"center")[[1]],probs=c(.5,.05,.95))
 			cv<-quantile(rstan::extract(fit,"v")[[1]],probs=c(.5,.05,.95))
 		}
@@ -207,7 +239,8 @@ est_genocl<-function(Gx=NULL,G0=NULL,G1=NULL,p0=NULL,p1=NULL,H=NULL,model="genot
 
 		if(dim(Gx)[2]==1){stop("at least two loci required for the hierarchical model")}
 
-		fit<-rstan::sampling(stanmodels$gencline_mix,data=dat)
+		fit<-rstan::sampling(stanmodels$gencline_mix,data=dat,
+			iter=n_iters,warmup=p_warmup,thin=n_thin)
 		cc<-t(apply(rstan::extract(fit,"center")[[1]],2,quantile,probs=c(.5,.05,.95)))
 		cv<-t(apply(rstan::extract(fit,"v")[[1]],2,quantile,probs=c(.5,.05,.95)))
 		sdc<-quantile(rstan::extract(fit,"sdc")[[1]],probs=c(.5,.05,.95))
@@ -224,7 +257,8 @@ est_genocl<-function(Gx=NULL,G0=NULL,G1=NULL,p0=NULL,p1=NULL,H=NULL,model="genot
 			init_ll<-lapply(1:n_chains, function(id) initf(chain_id = id))
 			dat<-list(L=dim(Gx)[2],N=dim(Gx)[1],G=Gx,H=H,P0=p0,P1=p1,sc=SDc,sv=SDv,
 			ploidy=pldat,init=init_ll)
-			fit<-rstan::sampling(stanmodels$gencline_sdk_mix,data=dat)
+			fit<-rstan::sampling(stanmodels$gencline_sdk_mix,data=dat,
+			iter=n_iters,warmup=p_warmup,thin=n_thin)
 			cc<-t(apply(rstan::extract(fit,"center")[[1]],2,quantile,probs=c(.5,.05,.95)))
 			cv<-t(apply(rstan::extract(fit,"v")[[1]],2,quantile,probs=c(.5,.05,.95)))
 
@@ -236,7 +270,8 @@ est_genocl<-function(Gx=NULL,G0=NULL,G1=NULL,p0=NULL,p1=NULL,H=NULL,model="genot
 			init_ll<-lapply(1:n_chains, function(id) initf(chain_id = id))		
 			dat<-list(L=1,N=length(Gx),G=Gx,H=H,P0=p0,P1=p1,sc=SDc,sv=SDv,ploidy=pldat,
 			init=init_ll)
-			fit<-rstan::sampling(stanmodels$gencline_one_mix,data=dat)
+			fit<-rstan::sampling(stanmodels$gencline_one_mix,data=dat,
+			iter=n_iters,warmup=p_warmup,thin=n_thin)
 			cc<-quantile(rstan::extract(fit,"center")[[1]],probs=c(.5,.05,.95))
 			cv<-quantile(rstan::extract(fit,"v")[[1]],probs=c(.5,.05,.95))
 		}
@@ -254,7 +289,8 @@ est_genocl<-function(Gx=NULL,G0=NULL,G1=NULL,p0=NULL,p1=NULL,H=NULL,model="genot
 
 		if(dim(Gx[[1]])[2]==1){stop("at least two loci required for the hierarchical model")}
 
-		fit<-rstan::sampling(stanmodels$gencline_gl_mix,data=dat)
+		fit<-rstan::sampling(stanmodels$gencline_gl_mix,data=dat,
+			iter=n_iters,warmup=p_warmup,thin=n_thin)
 		cc<-t(apply(rstan::extract(fit,"center")[[1]],2,quantile,probs=c(.5,.05,.95)))
 		cv<-t(apply(rstan::extract(fit,"v")[[1]],2,quantile,probs=c(.5,.05,.95)))
 		sdc<-quantile(rstan::extract(fit,"sdc")[[1]],probs=c(.5,.05,.95))
@@ -272,7 +308,8 @@ est_genocl<-function(Gx=NULL,G0=NULL,G1=NULL,p0=NULL,p1=NULL,H=NULL,model="genot
 			dat<-list(L=dim(Gx[[1]])[2],N=dim(Gx[[1]])[1],
 			GL0=Gx[[1]],GL1=Gx[[2]],GL2=Gx[[3]],H=H,P0=p0,P1=p1,sc=SDc,sv=SDv,
 			ploidy=pldat,init=init_ll)
-			fit<-rstan::sampling(stanmodels$gencline_sdk_gl_mix,data=dat)
+			fit<-rstan::sampling(stanmodels$gencline_sdk_gl_mix,data=dat,
+			iter=n_iters,warmup=p_warmup,thin=n_thin)
 			cc<-t(apply(rstan::extract(fit,"center")[[1]],2,quantile,probs=c(.5,.05,.95)))
 			cv<-t(apply(rstan::extract(fit,"v")[[1]],2,quantile,probs=c(.5,.05,.95)))
 
@@ -284,7 +321,8 @@ est_genocl<-function(Gx=NULL,G0=NULL,G1=NULL,p0=NULL,p1=NULL,H=NULL,model="genot
 			init_ll<-lapply(1:n_chains, function(id) initf(chain_id = id))		
 			dat<-list(L=1,N=length(Gx[[1]]),GL0=Gx[[1]],GL1=Gx[[2]],GL2=Gx[[3]],
 			H=H,P0=p0,P1=p1,sc=SDc,sv=SDv,ploidy=pldat,init=init_ll)
-			fit<-rstan::sampling(stanmodels$gencline_one_gl_mix,data=dat)
+			fit<-rstan::sampling(stanmodels$gencline_one_gl_mix,data=dat,
+			iter=n_iters,warmup=p_warmup,thin=n_thin)
 			cc<-quantile(rstan::extract(fit,"center")[[1]],probs=c(.5,.05,.95))
 			cv<-quantile(rstan::extract(fit,"v")[[1]],probs=c(.5,.05,.95))
 		}
@@ -301,7 +339,8 @@ est_genocl<-function(Gx=NULL,G0=NULL,G1=NULL,p0=NULL,p1=NULL,H=NULL,model="genot
 
 		if(dim(Gx)[2]==1){stop("at least two loci required for the hierarchical model")}
 
-		fit<-rstan::sampling(stanmodels$gencline_z_mix,data=dat)
+		fit<-rstan::sampling(stanmodels$gencline_z_mix,data=dat,
+			iter=n_iters,warmup=p_warmup,thin=n_thin)
 		cc<-t(apply(rstan::extract(fit,"center")[[1]],2,quantile,probs=c(.5,.05,.95)))
 		cv<-t(apply(rstan::extract(fit,"v")[[1]],2,quantile,probs=c(.5,.05,.95)))
 		sdc<-quantile(rstan::extract(fit,"sdc")[[1]],probs=c(.5,.05,.95))
@@ -318,7 +357,8 @@ est_genocl<-function(Gx=NULL,G0=NULL,G1=NULL,p0=NULL,p1=NULL,H=NULL,model="genot
 			init_ll<-lapply(1:n_chains, function(id) initf(chain_id = id))
 			dat<-list(L=dim(Gx)[2],N=dim(Gx)[1],Z=Gx,H=H,sc=SDc,sv=SDv,
 			ploidy=pldat,init=init_ll)
-			fit<-rstan::sampling(stanmodels$gencline_sdk_z_mix,data=dat)
+			fit<-rstan::sampling(stanmodels$gencline_sdk_z_mix,data=dat,
+			iter=n_iters,warmup=p_warmup,thin=n_thin)
 			cc<-t(apply(rstan::extract(fit,"center")[[1]],2,quantile,probs=c(.5,.05,.95)))
 			cv<-t(apply(rstan::extract(fit,"v")[[1]],2,quantile,probs=c(.5,.05,.95)))
 
@@ -330,7 +370,8 @@ est_genocl<-function(Gx=NULL,G0=NULL,G1=NULL,p0=NULL,p1=NULL,H=NULL,model="genot
 			init_ll<-lapply(1:n_chains, function(id) initf(chain_id = id))		
 			dat<-list(L=1,N=length(Gx),Z=Gx,H=H,sc=SDc,sv=SDv,ploidy=pldat,
 			init=init_ll)
-			fit<-rstan::sampling(stanmodels$gencline_one_z_mix,data=dat)
+			fit<-rstan::sampling(stanmodels$gencline_one_z_mix,data=dat,
+			iter=n_iters,warmup=p_warmup,thin=n_thin)
 			cc<-quantile(rstan::extract(fit,"center")[[1]],probs=c(.5,.05,.95))
 			cv<-quantile(rstan::extract(fit,"v")[[1]],probs=c(.5,.05,.95))
 		}

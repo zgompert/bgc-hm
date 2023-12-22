b14900ae179a698bd937974bbb3ed0c4e80de3f9
Zach Gompert <zach.gompert@usu.edu>
2023-12-22 16:13:25 -0700

    Updated documentation

--- R/bgchm-package.R
+++ R/bgchm-package.R
@@ -1,6 +1,6 @@
 #' The 'bgchm' package.
 #'
-#' @description A DESCRIPTION OF THE PACKAGE
+#' @description This is an R package for Bayesian analyses of population genomic data from hybrid zones, including Bayesian genomic cline analysis, estimation of hybrid indexes and ancestry classes, some geographic cline analyses, and accessory plotting functions. This package using Hamiltonian Monte Carlo (HMC) for sampling posterior distributions, with HMC sampling implemented via Stan. 
 #'
 #' @docType package
 #' @name bgchm-package
@@ -11,6 +11,7 @@
 #' @importFrom rstan sampling
 #'
 #' @references
+#' Gompert Z, et al. 2024. Bayesian hybrid zone analyses with Hamiltonian Monte Carlo in R. Manuscript in preparation
 #' Stan Development Team (2022). RStan: the R interface to Stan. R package version 2.21.7. https://mc-stan.org
 #'
 NULL
